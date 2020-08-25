229b167c169986751749012a34f24bbcd68b8a1b
CatButtes <catbuttes@outlook.com>
2020-08-25 20:27:24 +0100

    look in the right place for the ssh key

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "$VAULT_PASS" > ~/.vault_pass.txt
 mkdir ~/.ssh
-ansible-vault view --vault-password-file=~/.vault_pass.txt vars/ssh_key > ~/.ssh/id_rsa
+ansible-vault view --vault-password-file=~/.vault_pass.txt ssh/ansible_key > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 
 ansible-playbook --vault-password-file ~/.vault_pass.txt -i hosts/hosts.yml main.yml
