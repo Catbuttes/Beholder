8f6d9bbe6d109a0ad14f6bc02c5b873bf048a6e5
CatButtes <catbuttes@outlook.com>
2020-08-25 20:29:02 +0100

    Add become

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -5,4 +5,4 @@ mkdir ~/.ssh
 ansible-vault view --vault-password-file=~/.vault_pass.txt ssh/ansible_key > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 
-ansible-playbook --vault-password-file ~/.vault_pass.txt -i hosts/hosts.yml main.yml
+ansible-playbook --vault-password-file ~/.vault_pass.txt -i hosts/hosts.yml main.yml --become
