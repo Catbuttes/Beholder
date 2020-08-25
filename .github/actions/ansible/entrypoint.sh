ae7002ea94a92cb9e04f5c57b94460e06f559656
CatButtes <catbuttes@outlook.com>
2020-08-25 20:25:53 +0100

    fix a typo

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -5,4 +5,4 @@ mkdir ~/.ssh
 ansible-vault view --vault-password-file=~/.vault_pass.txt vars/ssh_key > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 
-ansible-playbook --vault-password-file ~/.vault_pass.txt -i hosts/hosts.tml main.yml
+ansible-playbook --vault-password-file ~/.vault_pass.txt -i hosts/hosts.yml main.yml
