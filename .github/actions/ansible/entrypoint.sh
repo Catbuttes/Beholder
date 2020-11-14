2de8a39d875bf7918ad4392ec9ba6fc0875dd508
CatButtes <catbuttes@outlook.com>
2020-11-14 13:20:09 +0000

    Move ansible-galaxy install to entrypoint

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -5,4 +5,5 @@ mkdir ~/.ssh
 ansible-vault view --vault-password-file=~/.vault_pass.txt ssh/ansible_key > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 
+ansible-galaxy collection install community.general
 ansible-playbook --vault-password-file ~/.vault_pass.txt -i hosts/hosts.yml main.yml --become
